214b4d8824090b8aa8370be1ae4f5460ecb54857
nnlinh070602 <nnl070602@gmail.com>
2023-12-14 22:23:27 +0700

    fix

--- project/app.py
+++ project/app.py
@@ -1,15 +1,13 @@
 from flask import Flask, render_template
 
+from project import app
 
-app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('login.html')
+    return render_template('index.html')
 
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
